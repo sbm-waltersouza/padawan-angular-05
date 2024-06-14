960048c1ec1ddc8bf5994feb721427f6ad3772fc
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-14 15:36:35 -0300

    Incluir comportamento no botão

--- base/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ base/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-pensamento',
@@ -16,7 +17,7 @@ export class PensamentoComponent implements OnInit {
     favorito: false
   }
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
   }
@@ -26,6 +27,18 @@ export class PensamentoComponent implements OnInit {
       return 'pensamento-g'
     }
     return 'pensamento-p'
+  }
+
+  mudarIconeFavorito(): string {
+    if(this.pensamento.favorito == false) {
+      return 'inativo'
+    }
+    return 'ativo'
+  }
+
+  atualizarFavoritos() {
+    this.service.mudarFavorito(this.pensamento).subscribe();
+    console.log(this.pensamento.favorito)
   }
 
 }
